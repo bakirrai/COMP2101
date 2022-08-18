45f0e6383459a6de68ece78d593dfc5b83ebc6e3
bakirrai <108446222+bakirrai@users.noreply.github.com>
2022-08-18 09:29:15 -0400

    Create sysReport mod.ps1

---
+++ powershell/sysReport mod.ps1
@@ -0,0 +1,11 @@
+﻿$param1=$args[0]
+gcim Win32_ComputerSystem 
+gwmi Win32_Processor 
+gwmi win32_videocontroller 
+gwmi win32_physicalmemory 
+gcim win32_operatingSystem
+$param2=$args[1]
+$networkInformation=Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE -ComputerName .
+
+
+
